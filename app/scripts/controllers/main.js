e7f756411ecb2115bd21fc9a024a5f1fbf8983cc
yytina <yang.hyejin@gmail.com>
2013-10-03 11:33:22 +0800

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('TickeyApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
