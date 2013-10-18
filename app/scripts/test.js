3970edbada33d1112df481e798187b6b75806708
yytina <yang.hyejin@gmail.com>
2013-10-18 16:31:16 +0800

    delete dummy html

--- app/scripts/test.js
+++
@@ -1,9 +0,0 @@
-var integerArray = [1,3,67,35,153,73,375];
-var maxNum=0;
-for (i=0;i<integerArray.length;i++){
-	if (maxNum<integerArray[i]){
-		maxNum=integerArray[i];
-	}
-}
-
-console.log(maxNum);
